4c55dcba8a7b39a4580a8b35fd868b1d48c8a47e
kampiiiii <dominik.kamp@geck.de>
2026-09-02 15:41:29 +0200

    Service Worker Cache fuer neue UI leeren

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tura-shell-v4";
+const CACHE_NAME = "tura-shell-v5";
 const APP_SHELL = ["/offline", "/manifest.webmanifest", "/icons/tura-icon-v2.svg"];
 const STATIC_DESTINATIONS = new Set(["font", "image", "script", "style"]);
 
